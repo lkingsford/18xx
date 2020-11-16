1464bc068199f0964d7401d229b3c37ab9986c4d
Lachlan Kingsford <lachlan@nerdygentleman.com>
2020-11-16 16:58:41 +1100

    Created 18Drac from 1889 and added market

---
+++ lib/engine/game/g_18_dracula.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require_relative '../config/game/g_18_dracula'
+require_relative '../step/g_1889/special_track'
+require_relative 'base'
+
+module Engine
+  module Game
+    class G18_dracula < Base
+      register_colors(black: '#37383a',
+                      orange: '#f48221',
+                      brightGreen: '#76a042',
+                      red: '#d81e3e',
+                      turquoise: '#00a993',
+                      blue: '#0189d1',
+                      brown: '#7b352a')
+
+      load_from_json(Config::Game::G18_dracula::JSON)
+
+      DEV_STAGE = :prealpha
+
+      GAME_LOCATION = 'Shikoku, Japan'
+      GAME_RULES_URL = 'http://dl.deepthoughtgames.com/1889-Rules.pdf'
+      GAME_DESIGNER = 'Yasutaka Ikeda (池田 康隆)'
+      GAME_PUBLISHER = :grand_trunk_games
+      GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/1889'
+
+      EBUY_PRES_SWAP = false # allow presidential swaps of other corps when ebuying
+      EBUY_OTHER_VALUE = false # allow ebuying other corp trains for up to face
+      HOME_TOKEN_TIMING = :operating_round
+
+      def operating_round(round_num)
+        Round::Operating.new(self, [
+          Step::Bankrupt,
+          Step::Exchange,
+          Step::DiscardTrain,
+          Step::G1889::SpecialTrack,
+          Step::BuyCompany,
+          Step::Track,
+          Step::Token,
+          Step::Route,
+          Step::Dividend,
+          Step::BuyTrain,
+          [Step::BuyCompany, blocks: true],
+        ], round_num: round_num)
+      end
+
+      def active_players
+        return super if @finished
+
+        current_entity == company_by_id('ER') ? [@round.company_seller] : super
+      end
+    end
+  end
+end
